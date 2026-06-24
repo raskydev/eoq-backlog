fc076127dc8352c7d964992f123cafac6ce6b245
Julia_Nakano <jumartinsnakano@gmail.com>
2026-06-23 22:27:38 -0300

    Colocando o metodo da bissecção

--- solver-eoq-backlog.c
+++ solver-eoq-backlog.c
@@ -2,9 +2,47 @@
 #include <math.h>
 
 
+//metodo da bisseção -- A.4
+double metodoBisseccao(double a, double b,
+                         double (*f)(double),
+                         double epsilon, int maxIter) {
+
+    if (f(a) * f(b) > 0) {
+        printf("Funcao nao convergiu\n");
+        return NAN;
+    }
+
+    double x, deltaX = (b - a) / 2.0;
+    int iter = 0;
+
+    do {
+        x = (a + b) / 2.0;
+        printf("%d\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n",
+               iter, a, b, f(a), f(b), f(x), deltaX);
+
+        if (fabs(f(x)) < epsilon && deltaX < epsilon) {
+            return x;
+        }
+
+        if (iter >= maxIter) {
+            return NAN;
+        }
+
+        if (f(a) * f(x) > 0) {
+            a = x;
+        } else {
+            b = x;
+        }
+
+        iter++;
+        deltaX /= 2.0;
+
+    } while (1);
+}
 // A.1 - entrada dos dados- Henrique
 
  // equação 14 --A.2
+
 
 double f(double q, double d, double s, double h, double pi){
 
